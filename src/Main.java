63e75a2aeb8c482f349a3502e50836902e9fc82d
113876-PAOLUCCI-AGUSTIN <113876@tecnicatura.frc.utn.edu.ar>
2024-03-21 16:06:52 -0300

    Variables creadas.

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,17 @@
+
+/*
+ * LC_III-guía-práctica-unidad-01-ejercicio-05
+ * 5. Ingresar la cantidad de horas trabajadas por un empleado y el sueldo que cobra por hora.
+ * Indique el total a cobrar teniendo en cuenta que si trabajó más de 180 horas las excedentes
+ * las cobra con un 50 % de aumento.
+ */
+
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+        Integer horasTrabajadas = input.nextInt();
+        Double sueldoPorHora = input.nextDouble();
+        Double totalACobrar;
     }
 }
